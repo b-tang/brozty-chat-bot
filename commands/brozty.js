92fdfa915d9ff9ec0928d8eff39dbe83e47e9fbc
Brian Tang <80483610+b-tang@users.noreply.github.com>
2021-07-12 08:25:27 -0700

    Delete brozty.js

--- commands/brozty.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-    name: 'brozty',
-    description: "Twitch Profile",
-    execute(client, channel, user, message, self, tags, tmi){
-        message.channel.say(channel, `@${tags.username}, heya!`);
-    }
-}
